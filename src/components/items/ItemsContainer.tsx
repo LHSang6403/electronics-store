7471316b8346c2df1ac9ef1e34804a9bdd223e29
LHSang <lhsang64.contact@gmail.com>
2023-12-17 00:54:02 +0700

    Sang - prerender bug

--- src/components/items/ItemsContainer.tsx
+++ src/components/items/ItemsContainer.tsx
@@ -1,6 +1,6 @@
 import Link from "next/link";
-import Product from "./Product";
-import Combobox from "../buttons/ComboBox";
+import Product from "@components/items/Product";
+import Combobox from "@components/buttons/ComboBox";
 import { readProducts } from "@app/_actions/productActions";
 import Image from "next/image";
 
