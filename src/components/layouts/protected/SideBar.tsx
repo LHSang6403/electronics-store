db19cf85a273e85c4ead7cebceda96c90f23f64f
LHSang <lhsang64.contact@gmail.com>
2024-02-05 23:38:55 +0700

    done

--- src/components/layouts/protected/SideBar.tsx
+++ src/components/layouts/protected/SideBar.tsx
@@ -25,6 +25,8 @@ export default function SideBar() {
     throw new Error("User not logged in.");
   }
 
+  const role = data?.data.role;
+
   const links = [
     {
       href: "/dashboard",
@@ -67,9 +69,7 @@ export default function SideBar() {
   return (
     <div className="bg-[#EEEEEE] xl:bg-white shadow xl:shadow-none w-[300px] xl:w-full h-[600px] xl:h-fit p-4 rounded-3xl flex flex-col justify-between">
       <div>
-        <h1 className="text-2xl xl:w-fit xl:mx-auto font-bold mt-1">
-          Dashboard
-        </h1>
+        <h1 className="text-2xl xl:w-fit xl:mx-auto font-bold mt-1">Dashboard</h1>
         <hr className="w-full xl:w-[60%] sm:w-full xl:mx-auto h-[2px] rounded-full bg-black my-2"></hr>
         {isSuccess && (
           <nav>
@@ -77,13 +77,11 @@ export default function SideBar() {
               {links.map((link, index) => (
                 <>
                   {link.permission === "admin" ? (
-                    data?.data.role === "admin" && (
+                    role === "admin" && (
                       <li
                         key={index}
                         className={`w-52 px-3 py-1 xl:text-center ${
-                          pathname === link.href
-                            ? "text-primary  bg-black rounded"
-                            : ""
+                          pathname === link.href ? "text-primary  bg-black rounded" : ""
                         }`}
                       >
                         <Link href={link.href}>{link.text}</Link>
@@ -93,9 +91,7 @@ export default function SideBar() {
                     <li
                       key={index}
                       className={`w-52 px-3 py-1 xl:text-center ${
-                        pathname === link.href
-                          ? "text-primary bg-black rounded"
-                          : ""
+                        pathname === link.href ? "text-primary bg-black rounded" : ""
                       }`}
                     >
                       <Link href={link.href}>{link.text}</Link>
@@ -107,10 +103,7 @@ export default function SideBar() {
           </nav>
         )}
       </div>
-      <button
-        className="w-full xl:w-[60%] xl:mx-auto bg-[#1F2937] rounded-lg text-white h-8"
-        onClick={() => {}}
-      >
+      <button className="w-full xl:w-[60%] xl:mx-auto bg-[#1F2937] rounded-lg text-white h-8" onClick={() => {}}>
         Sign out
       </button>
     </div>
